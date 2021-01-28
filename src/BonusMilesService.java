5ee05fdec5bfdf020e07a00dc052bce4e687e15a
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-01-28 22:04:57 +0300

    Оптимизация

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+  public class BonusMilesService {
+        public int calculate(int cost) {
+            int each =20;
+            int result = cost / each;
+            return result;
+        }
+    }
+
